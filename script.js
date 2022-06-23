702f18bed2e7f8cabe9e9cf5807c89961af3b955
Jsquaremay <jsquaremay007@gmail.com>
2022-06-23 18:54:11 +0200

    Exercice en Javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+ //"use strict";
+ //let user = "Mayinza",
+ //Univesity = 'upc';
+ t //const birth = "21 / 09 / 001";
+ //alert(user + ' ' + birth + ' ' + Univesity);
+
+ //let para = 'Bonjour je suis un para'
+ //console.log(para.length)
+
+ //const words = para.split(' ');
+ //console.log(words[])
+
+ //let a = '3.5px'
+
+ //console.log(parseInt('3px'))
+
+ //let now = new Date(2022, 5, 20);
+ //(now.getDate() + 2)
+ //alert(now)
+
+ /*function checkage(age) {
+     if (age >= 18) {
+         return true
+     } else {
+         return false
+     }
+ }
+
+ let age = prompt('Quel age avez vous ?', '')
+
+ if (checkage(age) === true) {
+     alert('Vous êtes majeur')
+ } else {
+     alert('vous êtes mineur')
+ }*/
+
+ let matrix = [
+     [1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]
+ ]
+ console.log(matrix[1][2])
